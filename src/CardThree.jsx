f4a2990cbe1f4b3ad0f443e2a776395795c84e8c
shubhamgujar01 <shubhamgujar791@gmail.com>
2023-12-02 13:01:33 +0530

    React Task guvi Oct1-4 2023

---
+++ src/CardThree.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+const CardThree = (props) => {
+
+    const serviceArr = ['Single User', '50GB Storage', 'Unlimited Public Projects', 'Community Access', 'Unlimited Private Projects',
+        'Dedicated Phone Support', 'Free Subdomain', 'Monthly Status Report']
+    return (
+        <div>
+            <Card style={{ width: '18rem', height: '400px', borderRadius: '10px', backgroundColor: 'white' }}>
+                <Card.Body>
+                    <Card.Subtitle className="mb-2 text-muted" style={{textAlign: 'center'}}>{props.subtitle}</Card.Subtitle>
+                    <Card.Title style={{textAlign: 'center'}}>{props.title}</Card.Title>
+                    <hr></hr>
+                    <Card.Text>
+                        <ul style={{ textAlign: 'left' }}>
+                            {
+                                serviceArr.map((elem, index) => {
+
+                                  
+
+                                        return (
+                                            <li style={{ listStyle: 'none' }}><span style={{marginRight: '10px'}}><i class="fa-solid fa-check"></i></span>{elem}</li>
+                                        )
+
+                                    
+                                    
+
+                                })
+
+                            }
+                        </ul>
+                        <Button variant="primary" style={{ width: '100%', borderRadius: '30px' }}>Button</Button>
+                    </Card.Text>
+
+                </Card.Body>
+            </Card>
+
+
+        </div>
+    )
+}
+
+export default CardThree;
