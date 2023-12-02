d850ff9e215498a9f872d11a779df848cce4c1ad
shubhamgujar01 <shubhamgujar791@gmail.com>
2023-12-02 13:24:14 +0530

    fixed

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
-import logo from './logo.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
-import CardOne from './CardOne';
-import CardTwo from './CardTwo';
-import CardThree from './CardThree';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import CardOne from "./CardOne";
+import CardTwo from "./CardTwo";
+import CardThree from "./CardThree";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./App.css";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-
-    <Container style={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%,-50%)'}}>
+    <Container
+      style={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%,-50%)",
+      }}
+    >
       <Row>
-       <Col> <CardOne title="$0/month" subtitle="FREE" /></Col>
-       <Col><CardTwo title="$9/month" subtitle="PLUS" /></Col> 
-       <Col><CardThree title="$49/month" subtitle="PRO" /></Col> 
+        <Col>
+          {" "}
+          <CardOne title="$0/month" subtitle="FREE" />
+        </Col>
+        <Col>
+          <CardTwo title="$9/month" subtitle="PLUS" />
+        </Col>
+        <Col>
+          <CardThree title="$49/month" subtitle="PRO" />
+        </Col>
       </Row>
-      </Container>
-   
-
-
+    </Container>
   );
 }
 
